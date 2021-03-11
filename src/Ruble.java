965a221b9c6b8d1618bb3a508aa0aaadd700da08
RenatTagitov <rena.tagirov@gmail.com>
2021-03-11 14:31:38 +0300

    Задача на манипуляцию с валютами

---
+++ src/Ruble.java
@@ -0,0 +1,11 @@
+public class Ruble extends Money {
+
+    Ruble(double amount) {
+        super(amount);
+    }
+
+    @Override
+    public String getCurrencyName() {
+        return "RUB";
+    }
+}
---
+++ src/USD.java
@@ -0,0 +1,12 @@
+public class USD extends Money {
+
+    USD(double amount) {
+        super(amount);
+    }
+
+    @Override
+    public String getCurrencyName() {
+        return "USD";
+    }
+
+}
